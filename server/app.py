eb75ecfd55da429312dff3303eb552dd1b753d70
menamerai <rphananh@gmail.com>
2023-11-05 03:50:31 -0500

    added ibm watson compatibility

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Api
 from llm import init_all_chain, get_abstract, summarize_and_similarity
 from scholar import Scholar
 
--- server/llm.py
+++ server/llm.py
@@ -5,6 +5,13 @@ from langchain.llms.cohere import Cohere
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import TokenTextSplitter
 from langchain.chains import LLMChain
+
+from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
+from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models import Model
+
+
 from scholar import Scholar
 
 load_dotenv()
@@ -14,12 +21,12 @@ COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
 def init_get_abstract_chain(llm, verbose=False):
     template = """
     You are a superintelligent AI trained for medical situations specifically.
-    You task is to receive the first 2000 tokens of a medical research paper 
-    and summarize the abstract of the paper in a maximum of 3 sentences.
-    Here is the query:
+    You task is to receive the 2000 tokens of a medical research PDF in plaintext 
+    and summarize it the whole thing based on the abstract of the paper.
+    Your summary should only be at most two sentences long.
+    Here is the text:
     {section}
-    Remember, you can only use the first 2000 tokens of the paper.
-    Your summary in a maximum of 3 sentences:
+    Your summary is:
     """
 
     prompt = PromptTemplate(
@@ -29,36 +36,19 @@ def init_get_abstract_chain(llm, verbose=False):
 
     return LLMChain(prompt=prompt, llm=llm, verbose=verbose)
 
-def init_summarize_chain(llm, verbose=False):
-    template = """
-    You are a superintelligent AI trained for medical situations specifically.
-    You task is to receive a piece of medical research PDF in plaintext and summarize it.
-    There is no limit on the length of the summary, but it should be concise and
-    contain the main points of the research, especially any insights on potential
-    pitfalls that a doctor should be aware of.
-    Here is the text:
-    {text}
-    Your summary:
-    """
-
-    prompt = PromptTemplate(
-        template=template,
-        input_variables=["text"]
-    )
-
-    return LLMChain(prompt=prompt, llm=llm, verbose=verbose)
-
-def init_similarity_chain(llm, verbose=False):
+def init_relevancy_chain(llm, verbose=False):
     template = """
     You are a superintelligent AI trained for medical situations specifically.
     You task is to receive a summary of a medical research paper and a specific
     patient's demographics information, and determine whether the research paper
-    is relevant to the patient's situation in percentage. If you cannot determine
-    the relevance, return ?%.
+    is relevant to the patient's situation in percentage based solely on
+    how the keywords in there match with the patients' demographic information.
+    If you cannot determine the relevance, return ?%.
     Here is the patient's information:
     {bioinfo}
     The paper's summary:
     {summary}
+    Remember, you should only return a number between 0%. and 100%., or ?%. if you cannot.
     The paper relevance to the patient's situation is:
     """
 
@@ -70,20 +60,36 @@ def init_similarity_chain(llm, verbose=False):
     return LLMChain(prompt=prompt, llm=llm, verbose=verbose)
 
 def init_all_chain(verbose=False):
+    params = {
+        GenParams.MAX_NEW_TOKENS: 50,
+        GenParams.MIN_NEW_TOKENS: 1,
+        GenParams.DECODING_METHOD: DecodingMethods.SAMPLE,
+        GenParams.TEMPERATURE: 0.5,
+        GenParams.TOP_K: 50,
+        GenParams.TOP_P: 1
+    }
+
+    model = Model(
+        model_id="google/flan-ul2",
+        credentials={
+            "apikey": "bGSfVEYB5i38neXBnEX7SiOela5wA59C06jFmfAxemxt",
+            "url": "https://us-south.ml.cloud.ibm.com"
+        },
+        params=params,
+        project_id="ba796275-8f1d-49a9-8a1c-b8650fa2bf39"
+    )
+
     llm_dict = {
         "get_abstract": Cohere(cohere_api_key=COHERE_API_KEY),
-        "summarize": Cohere(cohere_api_key=COHERE_API_KEY),
-        "similarity": Cohere(cohere_api_key=COHERE_API_KEY)
+        "relevancy": Cohere(cohere_api_key=COHERE_API_KEY)
     }
     
     get_abstract_chain = init_get_abstract_chain(llm=llm_dict["get_abstract"], verbose=verbose)
-    summarize_chain = init_summarize_chain(llm=llm_dict["summarize"], verbose=verbose)
-    similarity_chain = init_similarity_chain(llm=llm_dict["similarity"], verbose=verbose)
+    similarity_chain = init_relevancy_chain(llm=llm_dict["relevancy"], verbose=verbose)
 
     return {
         "get_abstract": get_abstract_chain,
-        "summarize": summarize_chain,
-        "similarity": similarity_chain
+        "relevancy": similarity_chain
     }
 
 def get_abstract(llm_chain, section):
@@ -93,14 +99,7 @@ def get_abstract(llm_chain, section):
         })
     raise ValueError()
 
-def summarize_text(llm_chain, text):
-    if llm_chain:
-        return llm_chain.run({
-            "text": text
-        })
-    raise ValueError()
-
-def get_similarity(llm_chain, bio_information, summary):
+def get_relevancy(llm_chain, bio_information, summary):
     if llm_chain:
         return llm_chain.run({
             "bioinfo": bio_information,
@@ -108,10 +107,10 @@ def get_similarity(llm_chain, bio_information, summary):
         })
     raise ValueError()
 
-def summarize_and_similarity(llm_dict, bio_information, text):
+def summarize_and_relevancy(llm_dict, bio_information, text):
     bio_information = ", ".join(bio_information)
-    summary = summarize_text(llm_chain=llm_dict["summarize"], text=text)
-    similarity = get_similarity(llm_chain=llm_dict["similarity"], bio_information=bio_information, summary=summary)
+    summary = get_abstract(llm_chain=llm_dict["get_abstract"], section=text)
+    similarity = get_relevancy(llm_chain=llm_dict["relevancy"], bio_information=bio_information, summary=summary)
     return summary, similarity
 
 def text_chunker(text, chunk_size=2000):
@@ -136,10 +135,8 @@ if __name__ == "__main__":
     paper = papers[0]
     # get the fist 2000 tokens after the first occurence of "abstract"
     text_chunked = get_abstract_text_chunk(text=paper["text"], chunk_size=1000)
-    abstract = get_abstract(llm_chain=chain_dict["get_abstract"], section=text_chunked)
-    print(f"Abstract: {abstract}")
-    # summarize, similarity = summarize_and_similarity(llm_dict=chain_dict, bio_information=demographics, text=text_chunked)
-    # print(f"Summary: {summarize}")
-    # print(f"Similarity: {similarity}")
+    abstract, relevancy = summarize_and_relevancy(llm_dict=chain_dict, bio_information=demographics, text=text_chunked)
+    print(abstract)
+    print(relevancy)
               
     
